4d6c4fc370db6fa942f1ea388832cc34eba04aaa
adventuresoflucylu <admin@adventuresoflucylu.com>
2015-11-17 11:29:27 -0800

    ReFactoring

---
+++ AdvllSrc/profiles/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='profile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=1200)),
+                ('description', models.TextField(default=b'description default')),
+                ('user', models.OneToOneField(null=True, blank=True, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='userStripe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('stripe_id', models.CharField(max_length=200, null=True, blank=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/Vid_N_Pics/Sites/adventures_of_lucy_lu/Git/advlucylu/tryDjango/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
